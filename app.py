676bb79db975befe96b822954024c5e73fd49bed
mikeill <mike@mzoo.org>
2025-10-10 09:58:08 -0500

    Add login.

--- app.py
+++ app.py
@@ -16,11 +16,17 @@ import json
 import os
 import stripe
 
-from flask import Flask, jsonify, request, render_template
+from flask import Flask, jsonify, request, render_template, redirect, flash
+""" url_for, Response, send_from_directory,  """
 from payment import payment
+import admin
 from dotenv import load_dotenv
+from flask_login import LoginManager, current_user, login_user, logout_user, login_required
 
 load_dotenv()
+
+
+
 # The library needs to be configured with your account's secret key.
 # Ensure the key is kept out of any version control system you might be using.
 stripe.api_key = os.environ["STRIPE_SECRET_KEY"]
@@ -29,6 +35,15 @@ stripe.api_key = os.environ["STRIPE_SECRET_KEY"]
 endpoint_secret = os.environ["STRIPE_WEBHOOK_SECRET"]
 
 app = Flask(__name__)
+app.config["SECRET_KEY"] = os.environ["SECRET_KEY"]
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+@login_manager.user_loader
+def load_user(id):
+	return admin.User.from_id(int(id))
+
 app.register_blueprint(payment, url_prefix="/payment")
 
 @app.route("/")
@@ -38,7 +53,25 @@ def index():
     # mutate the product to add the price
     for p in products:
          p["price"] = prices.get(p["id"])
-    return render_template("index.html", products=products)
+    return render_template("index.html", products=products, user=current_user)
+
+
+@app.route("/login")
+def login_get():
+	return render_template("login.html", user=current_user)
+
+@app.route("/login", methods=["POST"])
+def login_post():
+	user = admin.User.from_credentials(
+		request.form["email"],
+		request.form["password"]
+	)
+	if user:
+		login_user(user)
+		return redirect('/')
+	else:
+		flash("Invalid email or password")
+		return redirect("/")
 
 @app.route("/success")
 def success():
